88f394863e1f569f07185abaa40965ed09d9d2a9
arun <arun.chan86@gmail.com>
2023-08-14 21:33:25 +0530

    02- loading products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Product = () => {
+  const [products, getProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((data) => data.json())
+      .then((result) => getProducts(result));
+    console.log(products);
+  }, [products]);
+
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{marginBottom:'10px'}}>
+      <Card key={product.id} className='h-100'>
+        <div className='text-center'>
+        <Card.Img variant="top" src={product.image} style={{width:'100px',height:'130px'}}/>
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>$:{product.price}</Card.Text>          
+        </Card.Body>
+        <Card.Footer style={{backgroundColor:'white'}}>
+        <Button variant="primary">Add to Cart</Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ));
+
   return (
     <div>
       <h1>Product Dashboard</h1>
+      <div className="row">{cards}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
