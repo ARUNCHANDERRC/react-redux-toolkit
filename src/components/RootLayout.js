5bcf9c32fde91df887d9e94bf9670c3b38e364ed
arun <arun.chan86@gmail.com>
2023-08-15 12:08:40 +0530

    03- Add-Page-Routing

---
+++ src/components/RootLayout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import NavBar from './NavBarPanel'
+const RootLayout = () => {
+  return (
+    <div>
+      <NavBar />
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
+export default RootLayout
