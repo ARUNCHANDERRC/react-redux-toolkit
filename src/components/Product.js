729380ab1d47bce34e957467690bd8e2b5771db3
arun <arun.chan86@gmail.com>
2023-08-16 11:33:43 +0530

    04- create cartSlice and store

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useDispatch } from 'react-redux';
+import { add } from '../store/cartSlice';
 
 const Product = () => {
+ 
+const dispatch = useDispatch();
+
   const [products, getProducts] = useState([]);
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
@@ -11,18 +16,23 @@ const Product = () => {
     console.log(products);
   }, [products]);
 
+const addTocart = (product) =>{
+  // dispatch a add action
+  dispatch(add(product))
+}
+
   const cards = products.map((product) => (
     <div className="col-md-3" style={{marginBottom:'10px'}}>
       <Card key={product.id} className='h-100'>
         <div className='text-center'>
-        <Card.Img variant="top" src={product.image} style={{width:'100px',height:'130px'}}/>
+        <Card.Img variant="top" src={product.image} style={{width:'100px',height:'100px'}}/>
         </div>
         <Card.Body>
           <Card.Title>{product.title}</Card.Title>
           <Card.Text>$:{product.price}</Card.Text>          
         </Card.Body>
         <Card.Footer style={{backgroundColor:'white'}}>
-        <Button variant="primary">Add to Cart</Button>
+        <Button variant="primary" onClick={()=>addTocart(product)}>Add to Cart</Button>
         </Card.Footer>
       </Card>
     </div>
--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavBar from './NavBarPanel'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from './NavBarPanel';
+import { Provider } from 'react-redux';
+import store from '../store/store.js';
+
 const RootLayout = () => {
   return (
-    <div>
+    <>
+    <Provider store={store}>
       <NavBar />
 
       <main>
         <Outlet />
       </main>
-    </div>
-  )
-}
+      </Provider>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
